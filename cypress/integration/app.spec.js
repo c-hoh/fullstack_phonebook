2f176240b7eda6cefca521c6de0d297c27f41ec7
Christian Hohenfeld <r@hohenfeld.is>
2021-02-19 18:54:53 +0100

    Fix ports and adresses.

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,15 +1,15 @@
 describe('Phonebook application', function() {
   before(function() {
-    cy.request('localhost:3001/api/testing/reset')
+    cy.request('http://localhost:3001/api/testing/reset')
   }),
 
   it('can be opened', function() {
-    cy.visit('localhost:3001')
+    cy.visit('http://localhost:3001')
     cy.contains('Phonebook')
   }),
 
   it('can be used to add a new entry', function() {
-    cy.visit('localhost:3001')
+    cy.visit('http://localhost:3001')
     cy.get('#newName').type('Katze')
     cy.get('#newTel').type('123 323 123')
     cy.get('#submitPerson').click()
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.MODE === 'test'
   ? process.env.MONGODB_URI_TEST
   : process.env.MONGODB_URI
